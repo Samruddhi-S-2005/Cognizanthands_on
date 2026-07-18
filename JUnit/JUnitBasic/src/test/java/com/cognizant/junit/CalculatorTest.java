541adb1f16267e7798ca9df3cc173b5890e918b2
Samruddhi S <samruddhisamu2005@gmail.com>
2026-07-18 19:54:11 +0530

    Added JUnit Exercise 4 - AAA Pattern and Fixtures

--- JUnit/JUnitBasic/src/test/java/com/cognizant/junit/CalculatorTest.java
+++ JUnit/JUnitBasic/src/test/java/com/cognizant/junit/CalculatorTest.java
@@ -1,12 +1,18 @@
 package com.cognizant.junit;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
-    Calculator calculator = new Calculator();
+    Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
 
     @Test
     void testAddition() {

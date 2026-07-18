fcbc25965061804b6662588906d36cfa32417023
Samruddhi S <samruddhisamu2005@gmail.com>
2026-07-18 19:52:40 +0530

    Added JUnit Exercise 3 - Assertions

---
+++ JUnit/JUnitBasic/src/test/java/com/cognizant/junit/CalculatorTest.java
@@ -0,0 +1,33 @@
+package com.cognizant.junit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    void testAddition() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    void testSubtraction() {
+        assertEquals(2, calculator.subtract(5, 3));
+    }
+
+    @Test
+    void testMultiplication() {
+        assertEquals(12, calculator.multiply(3, 4));
+    }
+
+    @Test
+    void testAssertions() {
+        assertTrue(5 > 3);
+        assertFalse(3 > 5);
+        assertNotNull(calculator);
+        assertNull(null);
+    }
+}

d50ea3a35cf26cdaa6b81b5d8e10d208c3704505
Samruddhi S <samruddhisamu2005@gmail.com>
2026-07-21 16:09:11 +0530

    Completed REST Get Country by Code

--- SpringREST/spring-rest-handson/src/main/java/com/cognizant/springlearn/controller/CountryController.java
+++ SpringREST/spring-rest-handson/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -1,20 +1,28 @@
 package com.cognizant.springlearn.controller;
 
-import com.cognizant.springlearn.Country;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.springlearn.Country;
+import com.cognizant.springlearn.service.CountryService;
 
 @RestController
 public class CountryController {
 
-    @RequestMapping("/country")
+    @Autowired
+    private CountryService countryService;
+
+    @GetMapping("/country")
     public Country getCountryIndia() {
 
-        ApplicationContext context =
-                new ClassPathXmlApplicationContext("country.xml");
+        return countryService.getCountry("IN");
+    }
 
-        return context.getBean("country", Country.class);
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) {
+
+        return countryService.getCountry(code);
     }
 }

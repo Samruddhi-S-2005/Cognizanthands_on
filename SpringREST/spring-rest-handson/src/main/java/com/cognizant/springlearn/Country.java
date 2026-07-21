38bff160d2f10cc60354e228d8b1bcef6cea2941
Samruddhi S <samruddhisamu2005@gmail.com>
2026-07-21 15:52:11 +0530

    Completed REST Country Web Service

--- SpringREST/spring-rest-handson/src/main/java/com/cognizant/springlearn/Country.java
+++ SpringREST/spring-rest-handson/src/main/java/com/cognizant/springlearn/Country.java
@@ -10,22 +10,18 @@ public class Country {
     }
 
     public String getCode() {
-        System.out.println("Inside getCode()");
         return code;
     }
 
     public void setCode(String code) {
-        System.out.println("Inside setCode()");
         this.code = code;
     }
 
     public String getName() {
-        System.out.println("Inside getName()");
         return name;
     }
 
     public void setName(String name) {
-        System.out.println("Inside setName()");
         this.name = name;
     }
 

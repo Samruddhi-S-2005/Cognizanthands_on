240b7d327529a9009204904b81a005de21d22f80
Samruddhi S <samruddhisamu2005@gmail.com>
2026-07-21 15:42:09 +0530

    Completed Load Country from Spring Configuration XML

--- SpringREST/spring-rest-handson/src/main/java/com/cognizant/springlearn/SpringRestHandsonApplication.java
+++ SpringREST/spring-rest-handson/src/main/java/com/cognizant/springlearn/SpringRestHandsonApplication.java
@@ -2,12 +2,26 @@ package com.cognizant.springlearn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class SpringRestHandsonApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringRestHandsonApplication.class, args);
-	}
+    public static void main(String[] args) {
 
+        SpringApplication.run(SpringRestHandsonApplication.class, args);
+
+        displayCountry();
+    }
+
+    public static void displayCountry() {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        Country country = context.getBean("country", Country.class);
+
+        System.out.println(country);
+    }
 }

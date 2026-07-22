194037ea65ccccac52303fdda8dc457a3b96f868
Samruddhi S <samruddhisamu2005@gmail.com>
2026-07-22 10:43:48 +0530

    Completed ReactJS HOL 2

---
+++ studentapp/src/Components/Contact.js
@@ -0,0 +1,9 @@
+function Contact() {
+    return (
+        <div>
+            <h2>Welcome to Contact Component</h2>
+        </div>
+    );
+}
+
+export default Contact;
---
+++ studentapp/src/Components/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div>
+            <h2>Welcome to Home Component</h2>
+        </div>
+    );
+}
+
+export default Home;
